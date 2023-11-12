3975bcb0f7bef7ac36883a6d32e3e104551164ba
chimerako <65267814+chimerako@users.noreply.github.com>
2023-11-12 11:14:34 +0800

    Update 10-dependencies.sh

--- scripts/update/10-dependencies.sh
+++ scripts/update/10-dependencies.sh
@@ -1,62 +1,58 @@
 #!/bin/bash
 # Ensures that dependencies are installed and corrects them if that is not the case.
 
+# Function to add a repository and trigger an update
+add_repository() {
+    local repo=$1
+    local list_file=$2
+    if ! grep -s "$repo" "$list_file" 2> /dev/null | grep -q -v '^#'; then
+        echo_info "Adding $repo repo"
+        add-apt-repository -y ppa:"$repo" >> "${log}" 2>&1
+        trigger_apt_update=true
+    fi
+}
+
+# Check for add-apt-repository and install if missing
 if ! which add-apt-repository > /dev/null; then
-    apt_install software-properties-common # Ubuntu may require universe/mutliverse enabled for certain packages so we must ensure repos are enabled before deps are attempted to installed
+    apt_install software-properties-common
 fi
 
+# Add repositories based on OS and version
 if [[ $(_os_distro) == "ubuntu" ]]; then
-    if [[ $(_os_codename) == "jammy" ]]; then
-        if ! grep -s 'ubuntu-toolchain-r' /etc/apt/sources.list.d/ubuntu-toolchain-r-ubuntu-ppa-jammy.list 2> /dev/null | grep -q -v '^#'; then
-            echo_info "Adding toolchain repo"
-            add-apt-repository -y ppa:ubuntu-toolchain-r/ppa >> ${log} 2>&1
+    case $(_os_codename) in
+        "jammy")
+            add_repository "ubuntu-toolchain-r/ppa" "/etc/apt/sources.list.d/ubuntu-toolchain-r-ubuntu-ppa-jammy.list"
+            add_repository "ondrej/nginx" "/etc/apt/sources.list.d/ondrej-ubuntu-nginx-jammy.list"
+            ;;
+    esac
+
+    # Enable universe, multiverse, and restricted repositories
+    for repo in universe multiverse restricted; do
+        if ! grep "$repo" /etc/apt/sources.list | grep -q -v '^#'; then
+            echo_info "Enabling $repo repo"
+            add-apt-repository -y "$repo" >> "${log}" 2>&1
             trigger_apt_update=true
         fi
-    fi
-if [[ $(_os_distro) == "ubuntu" ]]; then
-    if [[ $(_os_codename) == "jammy" ]]; then
-        if ! grep -s 'ondrej/nginx' /etc/apt/sources.list.d/ondrej-ubuntu-nginx-jammy.list 2> /dev/null | grep -q -v '^#'; then
-            echo_info "Adding Nginx repo"
-            add-apt-repository -y ppa:ondrej/nginx >> ${log} 2>&1
-            trigger_apt_update=true
-        fi
-    fi
-    #Ignore a found match if the line is commented out
-    if ! grep 'universe' /etc/apt/sources.list | grep -q -v '^#'; then
-        echo_info "Enabling universe repo"
-        add-apt-repository -y universe >> ${log} 2>&1
-        trigger_apt_update=true
-    fi
-    if ! grep 'multiverse' /etc/apt/sources.list | grep -q -v '^#'; then
-        echo_info "Enabling multiverse repo"
-        add-apt-repository -y multiverse >> ${log} 2>&1
-        trigger_apt_update=true
-    fi
-    if ! grep 'restricted' /etc/apt/sources.list | grep -q -v '^#'; then
-        echo_info "Enabling restricted repo"
-        add-apt-repository -y restricted >> ${log} 2>&1
-        trigger_apt_update=true
-    fi
+    done
 elif [[ $(_os_distro) == "debian" ]]; then
-    if ! grep contrib /etc/apt/sources.list | grep -q -v '^#'; then
-        echo_info "Enabling contrib repo"
-        apt-add-repository -y contrib >> ${log} 2>&1
-        trigger_apt_update=true
-    fi
-    if ! grep -P '\bnon-free(\s|$)' /etc/apt/sources.list | grep -q -v '^#'; then
-        echo_info "Enabling non-free repo"
-        apt-add-repository -y non-free >> ${log} 2>&1
-        trigger_apt_update=true
-    fi
+    for repo in contrib non-free; do
+        if ! grep "$repo" /etc/apt/sources.list | grep -q -v '^#'; then
+            echo_info "Enabling $repo repo"
+            apt-add-repository -y "$repo" >> "${log}" 2>&1
+            trigger_apt_update=true
+        fi
+    done
 fi
+
+# Update package lists if needed
 if [[ $trigger_apt_update == "true" ]]; then
     apt_update
 fi
 
-#space-separated list of required GLOBAL SWIZZIN dependencies (NOT application specific ones)
+# Install dependencies
 dependencies="whiptail git sudo curl wget lsof fail2ban apache2-utils vnstat tcl tcl-dev build-essential dirmngr apt-transport-https bc uuid-runtime jq net-tools gnupg2 cracklib-runtime unzip ccze"
-
 apt_install "${dependencies[@]}"
 
+# Upgrade GCC for Jammy
 . /etc/swizzin/sources/functions/gcc
 GCC_Jammy_Upgrade
